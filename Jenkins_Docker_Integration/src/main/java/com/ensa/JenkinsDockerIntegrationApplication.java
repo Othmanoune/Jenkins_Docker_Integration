5542ea3e2862406de1183a862829ffb9f51100a8
othma <othma@Maroc>
2021-01-31 21:17:39 +0100

    Dockerfile

--- Jenkins_Docker_Integration/src/main/java/com/ensa/JenkinsDockerIntegrationApplication.java
+++ Jenkins_Docker_Integration/src/main/java/com/ensa/JenkinsDockerIntegrationApplication.java
@@ -2,10 +2,15 @@ package com.ensa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class JenkinsDockerIntegrationApplication {
 
+	 @RequestMapping("/")
+	  public String home() {
+	    return "Hello Docker World";
+	  }
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsDockerIntegrationApplication.class, args);
 	}
