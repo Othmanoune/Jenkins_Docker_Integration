4b47eded915efe48b2704b60ca5dfcc18b5fdff7
othma <othma@Maroc>
2021-02-01 01:12:21 +0100

    V6

--- Jenkins_Docker_Integration/src/test/java/com/ensa/AppTest.java
+++ Jenkins_Docker_Integration/src/test/java/com/ensa/AppTest.java
@@ -25,7 +25,7 @@ public class AppTest     extends TestCase
      */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite( AppTest.class ); 
         
     }
 
